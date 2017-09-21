369ba5049d57a6baed1bc75b8cc47a424b7f075f
Elko eppasa <eco.eppasa@gmail.com>
2017-09-21 13:03:36 +0700

    First COmmit

---
+++ src/main/java/com/elko/imd/ImdApplication.java
@@ -0,0 +1,12 @@
+package com.elko.imd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImdApplication.class, args);
+	}
+}
