6f27f9654aa73862423f03d311c3e48a571809c8
elko eppasa <eco.eppasa@gmail.com>
2017-09-30 13:18:12 +0700

    add javadoc documentation

--- src/main/java/com/elko/imd/handler/ExceptionHandler.java
+++ src/main/java/com/elko/imd/handler/ExceptionHandler.java
@@ -17,10 +17,36 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 /**
  *
+ * ExceptionHandler is a mechanism to handle runtime errors such as Http Request, ClassNotFound, IO, SQL, Remote etc.. 
+ * The exception handling in java is one of the powerful mechanism to handle the runtime errors so that normal flow of the application can be maintained.
+ * There are mainly two types of exceptions: checked and unchecked where error is considered as unchecked exception. The sun microsystem says there are three types of exceptions:
+ * 
+ * <br> <h5>1. Checked Exception</h5>
+ *      The classes that extend Throwable class except RuntimeException and Error are known as checked exceptions 
+ *      e.g.IOException, SQLException etc. Checked exceptions are checked at compile-time.
+ * <br> <h5>2. Unchecked Exception</h5>
+ *  The classes that extend RuntimeException are known as unchecked exceptions e.g. ArithmeticException, NullPointerException, ArrayIndexOutOfBoundsException etc. 
+ *      Unchecked exceptions are not checked at compile-time rather they are checked at runtime.
+ * <br> <h5>3. Error </h5>
+ *  Error is irrecoverable e.g. OutOfMemoryError, VirtualMachineError, AssertionError etc.
+ * 
  * @author elko
+ * @since '2017-09-25'
+ * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/ExceptionHandler.html">ExceptionHandler</a>
+ * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/bind/annotation/ControllerAdvice.html">ControllerAdvice</a>
+ * @see <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/servlet/mvc/method/annotation/ResponseEntityExceptionHandler.html">ResponseEntityExceptionHandler</a>
  */
 @ControllerAdvice
 public class ExceptionHandler extends ResponseEntityExceptionHandler{
+    
+    /**
+     * handeGeneral is method for handle any exception thats not define on other method in this class     
+     * @param ex is return value of exception class that gets from exception handler 
+     * @param request is http response of web request 
+     * @return <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html">ResponseEntity</a>
+     * is meant to represent the entire HTTP response. You can control anything that goes into it: status code, headers, and body.
+     * 
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler({ Exception.class })
     protected ResponseEntity handleGeneral(
       Exception ex, WebRequest request) {
@@ -28,6 +54,15 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler{
           new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
     
+    /**
+     * 
+     * handeNotFound is method for throwing exception if services class have NullPointerException error
+     * 
+     * @param ex is return value of exception class that gets from exception handler 
+     * @param request is http response of web request 
+     * @return <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html">ResponseEntity</a>
+     * is meant to represent the entire HTTP response. You can control anything that goes into it: status code, headers, and body.
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler({NullPointerException.class})
     protected ResponseEntity handleNotFound(
       Exception ex, WebRequest request) {
@@ -35,13 +70,31 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler{
           new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
     
+    /**
+     * 
+     * handleJson is method for throwing exception if raw input of  JSON has a invalid format
+     * 
+     * @param ex is return value of exception class that gets from exception handler 
+     * @param request is http response of web request 
+     * @return <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html">ResponseEntity</a>
+     * is meant to represent the entire HTTP response. You can control anything that goes into it: status code, headers, and body.
+     */
     @org.springframework.web.bind.annotation.ExceptionHandler({JsonParseException.class})
-    protected ResponseEntity handleGSon(
+    protected ResponseEntity handleJSon(
       Exception ex, WebRequest request) {
         return handleExceptionInternal(ex, "Invalid JSON Format!, Please Check again! ", 
           new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
     
+    /**
+     * 
+     * handleValidation is method for throwing exception if input value has a invalid format. such as email format, number format, and etc.. 
+     * 
+     * @param ex is return value of exception class that gets from exception handler 
+     * @param request is http response of web request 
+     * @return <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html">ResponseEntity</a>
+     * is meant to represent the entire HTTP response. You can control anything that goes into it: status code, headers, and body.
+    */
     @org.springframework.web.bind.annotation.ExceptionHandler({ConstraintViolationException.class})
     protected ResponseEntity handleValidation(
       Exception ex, WebRequest request) {
@@ -49,6 +102,15 @@ public class ExceptionHandler extends ResponseEntityExceptionHandler{
           new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
     
+    /**
+     *
+     * hibernateSession is method for throwing exception if session has been closed.
+     * 
+     * @param ex is return value of exception class that gets from exception handler 
+     * @param request is http response of web request 
+     * @return <a href="https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html">ResponseEntity</a>
+     * is meant to represent the entire HTTP response. You can control anything that goes into it: status code, headers, and body.
+     */
      @org.springframework.web.bind.annotation.ExceptionHandler({HibernateJdbcException.class})
     protected ResponseEntity hibernateSession(
       Exception ex, WebRequest request) {
